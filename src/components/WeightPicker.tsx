0899950c911de11c87ef2b96dbad7dee06e560fa
HenrikDK2 <henrik265@outlook.dk>
2022-06-27 20:33:03 +0200

    Reuse click function

--- src/components/WeightPicker.tsx
+++ src/components/WeightPicker.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { AiOutlineBold } from "react-icons/ai";
 import { BiFont } from "react-icons/bi";
 import { useAppDispatch, useAppSelector } from "../redux/store";
+import { FontWeight } from "../types";
 import { updateContentWithNewProps } from "../utils";
 import { HeadingContent } from "./Heading";
 import { LinkContent } from "./Link";
@@ -14,41 +15,31 @@ export const WeightPicker: FC<IWeightPickerProps> = ({ content }) => {
   const { editContentIndex } = useAppSelector((state) => state.admin);
   const dispatch = useAppDispatch();
 
+  const handleClick = (weight: FontWeight) => {
+    dispatch(updateContentWithNewProps(content, { weight }, editContentIndex));
+  };
+
   return (
     <div>
       <label htmlFor="weight">Weight</label>
+
       <div className="flex gap-2 mt-2" id="weight">
         <button
-          onClick={() => {
-            dispatch(
-              updateContentWithNewProps(
-                content,
-                { weight: "normal" },
-                editContentIndex
-              )
-            );
-          }}
+          onClick={() => handleClick("normal")}
           className={`flex justify-center items-center transition-all w-8 h-8 rounded-full hover:shadow-lg border-slate-100  ${
             content.props.weight === "normal" && "border-2 shadow-lg"
           }`}
         >
-          <BiFont size={20} />
+          <BiFont aria-label="Normal Weight" size={20} />
         </button>
+
         <button
-          onClick={() => {
-            dispatch(
-              updateContentWithNewProps(
-                content,
-                { weight: "bold" },
-                editContentIndex
-              )
-            );
-          }}
+          onClick={() => handleClick("bold")}
           className={`flex justify-center items-center transition-all w-8 h-8 rounded-full hover:shadow-lg border-slate-100  ${
             content.props.weight === "bold" && "border-2 shadow-lg"
           }`}
         >
-          <AiOutlineBold size={20} />
+          <AiOutlineBold aria-label="Bold Weight" size={20} />
         </button>
       </div>
     </div>
